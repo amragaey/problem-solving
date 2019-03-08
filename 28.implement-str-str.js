eba9c53ad63f3a868a2ec23a6341855fb70ae273
amragaey <amragaey@gmail.com>
2019-03-08 16:27:04 +0200

    str-str like indexOf

---
+++ 28.implement-str-str.js
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode id=28 lang=javascript
+ *
+ * [28] Implement strStr()
+ *
+ * https://leetcode.com/problems/implement-strstr/description/
+ *
+ * algorithms
+ * Easy (31.29%)
+ * Total Accepted:    387.1K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '"hello"\n"ll"'
+ *
+ * Implement strStr().
+ * 
+ * Return the index of the first occurrence of needle in haystack, or -1 if
+ * needle is not part of haystack.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: haystack = "hello", needle = "ll"
+ * Output: 2
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: haystack = "aaaaa", needle = "bba"
+ * Output: -1
+ * 
+ * 
+ * Clarification:
+ * 
+ * What should we return when needle is an empty string? This is a great
+ * question to ask during an interview.
+ * 
+ * For the purpose of this problem, we will return 0 when needle is an empty
+ * string. This is consistent to C's strstr() and Java's indexOf().
+ * 
+ */
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+
+  if (needle.length == 0) return 0;
+  if (needle.length > haystack.length) return -1;
+
+  for (let i = 0; i < haystack.length; i++) {
+    let searchString = haystack.substr(i, needle.length);
+    if (searchString === needle) {
+      return i;
+    }
+  }
+
+  return -1;
+
+};
