60833610905dd5f62990678bb4ec31cc65b93b42
amragaey <amragaey@gmail.com>
2019-03-09 22:26:57 +0200

    max sub array - linear

---
+++ 53.maximum-subarray.js
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=53 lang=javascript
+ *
+ * [53] Maximum Subarray
+ *
+ * https://leetcode.com/problems/maximum-subarray/description/
+ *
+ * algorithms
+ * Easy (42.85%)
+ * Total Accepted:    471K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[-2,1,-3,4,-1,2,1,-5,4]'
+ *
+ * Given an integer array nums, find the contiguous subarray (containing at
+ * least one number) which has the largest sum and return its sum.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [-2,1,-3,4,-1,2,1,-5,4],
+ * Output: 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ * 
+ * 
+ * Follow up:
+ * 
+ * If you have figured out the O(n) solution, try coding another solution using
+ * the divide and conquer approach, which is more subtle.
+ * 
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+  if (!nums.length) return 0;
+  if (nums.length == 1) return nums[0];
+
+  let max = -Infinity;
+  let sum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    sum = Math.max(sum, nums[i]);
+
+    if (sum > max) {
+      max = sum;
+    }
+  }
+
+  return max;
+};
